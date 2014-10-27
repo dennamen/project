a9c0896a47f1b2da74ccea7c4b8adef4782483b0
Dennamen <daemonshi@mail.ru>
2014-10-27 12:05:56 +0300

    iddqd

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
  * Created by student on 20.10.2014.
  */
 public class AddressBook {
-    ArrayList<Address> list= new ArrayList<Address>();
+    private ArrayList<Address> list= new ArrayList<Address>();
 
     public void add (Address a){
         list.add(a);
     }
-    public void find (String n){
-        for (int i = 0; i<=list.size();i++){
-            if (list.get(i).name == n){
-                System.out.println(list.get(i));
+
+    public ArrayList<Integer> find (String n){
+        ArrayList<Integer> z = new ArrayList<Integer> ();
+
+        for (int i = 0; i<list.size();i++){
+            if (list.get(i).name.equals(n)){
+                //print1(list.get(i));
+                z.add(i);
             }
         }
+        return z;
     }
 
     public Address findind(int x){
@@ -22,14 +27,25 @@ public class AddressBook {
     }
 
     public void print (){
-        for (int i = 0; i<=list.size();i++){
-                System.out.println(list.get(i));
+        for (int i = 0; i<list.size();i++){
+                print1(i, list.get(i));
         }
     }
+
+    public void print1 (int index, Address entry){
+        System.out.println("Индекс: "+index+", Имя: "+entry.name+", Телефон: "+entry.phone+", Почта: "+entry.email);
+    }
+
     public void delete (int i){
-        list.remove(findind(i));
+        list.remove(i);
     }
     public void count (){
         System.out.println(list.size());
+    }
+
+    public void edit(int index, String name, String phone, String email) {
+        list.get(index).name=name;
+        list.get(index).phone=phone;
+        list.get(index).email=email;
     }
 }
--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -8,21 +9,20 @@ public class Manager {
     public static void main(String[] args){
         AddressBook book = new AddressBook();
 
-        System.out.println("1.Добавление записи");
-        System.out.println("2.Поиск");
-        System.out.println("3.Вывод всех записей");
-        System.out.println("4.Удаление");
-        System.out.println("5.Изменение");
-        System.out.println("6.Выход из программы");
+
         Scanner in = new Scanner(System.in);
         String n;
         String p;
         String e;
         int i;
-        Address address;
         int a;
         while (true) {
-            System.out.println("Oyvey!");
+            System.out.println("1.Добавление записи");
+            System.out.println("2.Поиск по имени");
+            System.out.println("3.Вывод всех записей");
+            System.out.println("4.Удаление");
+            System.out.println("5.Изменение");
+            System.out.println("6.Выход из программы");
             a = in.nextInt();
             switch (a) {
                 case 1:
@@ -37,10 +37,17 @@ public class Manager {
                 case 2:
                     System.out.println("Имя:");
                     String z = in.next();
-                    book.find(z);
+                    ArrayList<Integer> found = book.find(z);
+                    for (i=0; i<found.size();i++){
+                        System.out.println(i);
+                        Integer index = found.get(i);
+                        book.print1(index, book.findind(index));
+                    }
+                    // todo: вывод найденных записей
                     break;
                 case 3:
                     book.print();
+
                     break;
                 case 4:
                     System.out.println("Индекс:");
@@ -56,12 +63,12 @@ public class Manager {
                      p = in.next();
                     System.out.println("Почта:");
                      e = in.next();
-                    book.list.get(i).name=n;
-                    book.list.get(i).phone=p;
-                    book.list.get(i).email=e;
+                    book.edit(i, n, p, e);
+
                     break;
                 case 6:
-                 break;
+                    return;
+
             }
         }
 
